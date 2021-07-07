2a8bcf407b10bbcb3a8b9d532990e0ac530c257e
VanJackal <v@njackal.com>
2021-07-07 13:34:03 -0500

    added minecraft server status check

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const {EC2Client, StopInstancesCommand, StartInstancesCommand, ModifyVolumeAttributeCommand} = require('@aws-sdk/client-ec2');
+const {EC2Client, StopInstancesCommand, StartInstancesCommand} = require('@aws-sdk/client-ec2');
 const client = new EC2Client({ region: "us-east-2" });
+const mcPing = require('minecraft-ping');
 
-class ServerManager{
-    constructor(instanceID,serverURI){
+class Server{
+    #serverTimeout;
+
+    constructor(instanceID,serverURI,serverPort){
         this.instanceID = instanceID;
         this.serverURI = serverURI;
+        this.serverPort = serverPort;
+        this.#serverTimeout = null;
     }
 
     stopServer = async function(){
@@ -18,8 +23,38 @@ class ServerManager{
         const response = await client.send(command);
         console.log(response);
     }
+
+    getServerStatus = function(cb = this.processStatus){//if cb is given args (null,null) then the instance isnt up
+        if(this.getInstanceStatus){
+            mcPing.ping_fe({host: this.serverURI, port: this.serverPort},cb);
+        } else {
+            cb(null,null);
+        }
+    }
+
+    processStatus = function(err,res){
+        if(!err && res){
+            console.log(res);
+        } else if (!res && err){
+            console.log("Error in Status Processing:")
+            console.log(err)
+        } else {
+            this.endTimeout();
+        }
+    }
+
+    endTimeout = function(){
+        if(this.#serverTimeout){
+            clearTimeout(this.#serverTimeout);
+            this.#serverTimeout = null;
+        }
+    }
+
+    getInstanceStatus = function(){
+        return true;
+    }
 }
 
 module.exports = {
-    ServerManager
+    Server
 }
