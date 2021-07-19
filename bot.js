544c1a5fbe115221ed0a359659d039eb89446108
VanJackal <v@njackal.com>
2021-07-18 23:57:34 -0500

    added start/stop commands and user/role verifcation

--- bot.js
+++ bot.js
@@ -5,6 +5,7 @@ const PREFIX = process.env.PREFIX;
 
 const {ServerManager} = require("./serverManager");
 const serverManager = new ServerManager("./servers.json");
+const config = require("./config.json");
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -23,14 +24,54 @@ client.on('message', message => {
         case 'status':
             commandStatus(message,args[0]);
             break;
+        case 'stop':
+            commandStopInstance(message,args[0]);
+            break;
+        case 'start':
+            commandStartInstance(message,args[0]);
+            break;
         default:
             message.channel.send(`\`${command}\` is not a recognized command.`)
     }
 });
 
-function commandStatus(message,HRID){
+async function commandStatus(message,HRID){
     status = serverManager.getStatusFromHRID(HRID);
     message.channel.send(status);
+}
+
+async function commandStartInstance(message,HRID){
+    if(checkForPerms(message.author)){
+        startMessage = await serverManager.startInstanceFromHRID(HRID);
+        message.channel.send(startMessage);
+    } else {
+        return "You do not have permission to do this"
+    }
+}
+
+async function commandStopInstance(message,HRID){
+    if(checkForPerms(message.author)){
+        stopMessage = await serverManager.stopInstanceFromHRID(HRID);
+        message.channel.send(stopMessage);
+    } else {
+        message.channel.send("You do not have permission to do this");
+    }
+}
+
+function checkForPerms(author){
+    let ret = false; 
+    config.roles.forEach(roleID => {
+        console.log(roleID);
+        if(author.roles.cache.has(roleID)){
+            ret = true;
+            return;
+        }
+    });
+    if(!ret && config.admins.includes(author.id)){            
+        ret = true;
+    }
+
+    return ret;
 }
 
 client.login(process.env.TOKEN);
